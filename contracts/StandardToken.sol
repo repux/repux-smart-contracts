a438291f3ed50e58a5085b006dc351bee41c60e7
Przemysław Telążka <przemyslaw.telazka@workhq.com>
2018-05-23 14:04:52 +0200

    feat(smart-contract): new Registry and DataProduct events

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 import "./ERC20.sol";
 import "./ERC20Basic.sol";
--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ require('babel-polyfill');
 module.exports = {
     networks: {
         development: {
-            host: "local.dev.api.repux",
+            host: "192.168.99.100",
             port: 8545,
             network_id: "*"
         },
