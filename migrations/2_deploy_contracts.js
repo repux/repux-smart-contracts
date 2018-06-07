f5c8df55a28e5898f91997066c4b2238d26e4f4a
Przemyslaw Kocznur <pkocznur@gmail.com>
2018-06-07 15:32:00 +0200

    REPUX-636 Deploy marketplace contracts to rinkeby net

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,5 +11,13 @@ async function deploy(deployer, network, accounts) {
 }
 
 module.exports = function (deployer, network, accounts) {
-    deployer.then(() => deploy(deployer, network, accounts));
+    deployer.then(() => {
+        if (network === 'rinkeby') {
+            deployer.deploy(Registry, '0xfa19d4e302336d61b895ea3b26bf4864bdd1d8ab');
+        }
+        else {
+            deploy(deployer, network, accounts)
+        }
+    }
+    );
 };
